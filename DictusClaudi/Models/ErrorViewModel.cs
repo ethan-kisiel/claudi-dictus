975ce254c4109cb1295b7a2e3b829b29ce262ae1
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2023-06-27 14:49:41 -0400

    initial commit

---
+++ DictusClaudi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DictusClaudi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
